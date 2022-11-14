f0984e172f2ebcb783fcdb317993a8b2928d91e6
unknown <ido.bandd@gmail.com>
2022-11-14 17:51:10 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+const developer = {
+    name:"Ido Band",
+    version: "0.0.0",
+    desciption:"This calculator should help you do math, math is math"
+};
+
+// func requested on part I sec. 9 
+function developerDetails() {
+    alert("Developer's Name: Ido band \nVesion: 0.0.0 \ndescription: math is math");
+};
+
+// func requested on part I sec. 10
+function displayButtonInfo(id) {
+    alert(id);
+};
