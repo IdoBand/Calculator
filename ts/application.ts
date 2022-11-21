79a929fc57b30125f3da083f73809d66b22417fd
unknown <ido.bandd@gmail.com>
2022-11-21 21:45:54 +0200

    Working on part III

--- ts/application.ts
+++ ts/application.ts
@@ -3,21 +3,27 @@
 //     version: "1.0.0",
 //     desciption:"This calculator should help you do math, math is math"
 // };
+
+
 // these 2 functios are just shortcuts to select items
 const qasApp = document.querySelectorAll.bind(document);
 const byIdApp = document.getElementById.bind(document);
+
+
 // these 2 functios are just shortcuts to make it easier to render
 const hideMains = () => qasApp('main').forEach(e => e.style.display = 'none');
-const showSelectedMains = (modeState) => {
+const showSelectedMains = (modeState) => { 
     if (modeState.scientific === true) {
-        byIdApp('scientific').style.display = 'block';
-    }
+        byIdApp('scientific').style.display = 'block';}
     if (modeState.history === true) {
-        byIdApp('operations-log').style.display = 'block';
-    }
-};
+        byIdApp('operations-log').style.display = 'block';}
+  };
+
 // const subSettings = () => document.querySelector.bind('.sub-settings').forEach(e => e.style.display = 'none')
 // byIdApp('hi').style.display = 'none';
+
+
+
 // which modes are on/off
 let modeState = {
     basic: true,
@@ -25,8 +31,9 @@ let modeState = {
     history: false,
     displaylight: false,
 };
+
 // display screen light
-let lightBtn = byIdApp('light');
+let lightBtn = byIdApp('light')
 lightBtn.addEventListener('click', () => {
     let display = byIdApp('display');
     if (modeState.displaylight === false) {
@@ -38,39 +45,39 @@ lightBtn.addEventListener('click', () => {
         modeState.displaylight = false;
     }
 });
+
 //histoy-mode
-let historyModeBtn = byIdApp('history');
+let historyModeBtn = byIdApp('history')
 historyModeBtn.addEventListener('click', () => {
     if (modeState.history === false) {
         modeState.history = true;
-        historyModeBtn.style.backgroundColor = 'rgb(230, 167, 9)';
-    }
-    else {
-        modeState.history = false;
-        historyModeBtn.style.backgroundColor = 'rgb(95, 95, 219)';
-    }
+        historyModeBtn.style.backgroundColor = 'rgb(230, 167, 9)';}
+    else {modeState.history = false;
+        historyModeBtn.style.backgroundColor = 'rgb(95, 95, 219)';}
     render(modeState);
 });
+
 //basic-mode
-let basicModeBtn = byIdApp('basic-mode');
+let basicModeBtn = byIdApp('basic-mode')
 basicModeBtn.addEventListener('click', () => {
     modeState.scientific = false;
     // if (modeState.scientific === true) {};
     scientificModeBtn.style.backgroundColor = 'rgb(95, 95, 219)';
     render(modeState);
 });
+
 //scientific-mode
-let scientificModeBtn = byIdApp('scientific-mode');
+let scientificModeBtn = byIdApp('scientific-mode')
 scientificModeBtn.addEventListener('click', () => {
     modeState.scientific = true;
     scientificModeBtn.style.backgroundColor = 'rgb(230, 167, 9)';
     render(modeState);
 });
+
 // a function to change the page background color
 function changeBackgroundColor(e) {
     document.body.style.backgroundColor = e.target.id;
-}
-;
+    };
 // dark mode function
 function darkMode() {
     let bodyElement = document.body;
@@ -78,13 +85,17 @@ function darkMode() {
         bodyElement.classList.add("dark");
     }
     else {
-        bodyElement.classList.remove("dark");
+          bodyElement.classList.remove("dark");
     }
-}
-;
+
+};
+
+
+
 function render(s) {
     hideMains();
     showSelectedMains(s);
-}
-;
-document.addEventListener('DOMContentLoaded', () => render(modeState));
+};
+
+
+document.addEventListener('DOMContentLoaded', () => render(modeState))
--- ts/calculator.ts
+++ ts/calculator.ts
@@ -1,6 +1,8 @@
 // these 2 functios are just shortcuts to select items
 const qasCalc = document.querySelectorAll.bind(document);
 const byIdCalc = document.getElementById.bind(document);
+
+
 let mathValues = {
     scientific: false,
     firstOperand: '',
@@ -8,67 +10,74 @@ let mathValues = {
     currentOperator: '',
     valueCalculated: ''
 };
+
 const display = byIdCalc('display');
+
 let digitBtns = qasCalc('.digit');
+
+
 digitBtns.forEach(button => {
     button.addEventListener('click', (e) => {
         digitClick(e);
     });
 });
-function digitClick(e) {
-    if (mathValues.scientific === true) { // scientific mode in ON
+
+function digitClick (e) {
+    if (mathValues.scientific === true) {            // scientific mode in ON
         display.innerText += e.target.innerText;
     }
-    else { // scientific mode in OFF
-        if (mathValues.currentOperator === '') {
+    else {                                           // scientific mode in OFF
+        if (mathValues.currentOperator === ''){
             display.innerText += e.target.innerText;
-            mathValues.firstOperand = display.innerText;
+            mathValues.firstOperand = display.innerText
         }
         else if (mathValues.currentOperator !== '') {
-            display.innerText += e.target.innerText;
-            mathValues.secondOperand += +e.target.innerText;
+        display.innerText += e.target.innerText;
+        mathValues.secondOperand += +e.target.innerText;
         }
         else {
             display.innerText += e.target.innerText;
             mathValues.secondOperand += e.target.innerText;
         }
     }
-}
-;
-let operators = ['+', '-', '/', '*'];
-let operatorBtns = qasCalc('.operator');
+
+};
+
+let operators: string[] = ['+','-','/','*'];
+let operatorBtns = qasCalc('.operator')
+
 operatorBtns.forEach(button => {
     button.addEventListener('click', (e) => {
-        let lastChar = display.innerText.charAt(display.innerText.length - 1);
-        operatorClick(e, lastChar);
+        let lastChar = display.innerText.charAt(display.innerText.length-1);
+            operatorClick(e, lastChar);
     });
 });
-function operatorClick(e, lastChar) {
-    if (mathValues.scientific === true) { // scientific mode in ON
-        console.log(operatorBtns);
+
+function operatorClick (e, lastChar: string) {
+    if (mathValues.scientific === true) {            // scientific mode in ON
+        console.log(operatorBtns)
         display.innerText += e.target.innerText;
     }
     else {
-        if (mathValues.currentOperator === '') { // operator not assigned yet.
+        if (mathValues.currentOperator === ''){                // operator not assigned yet.
             display.innerText += e.target.id;
             mathValues.currentOperator = e.target.innerText;
         }
-        else if (mathValues.currentOperator !== '' && operators.includes(lastChar)) { // an operator has already been clicked once.
-            display.innerText = display.innerText.slice(0, -1); // delete operator from display.
-            display.innerText += e.target.id; // add new operator to display.
-            mathValues.currentOperator = e.target.innerText; // set new currentOperator.
+        else if (mathValues.currentOperator !== '' && operators.includes(lastChar)){ // an operator has already been clicked once.
+            display.innerText = display.innerText.slice(0,-1);  // delete operator from display.
+            display.innerText += e.target.id;                         // add new operator to display.
+            mathValues.currentOperator = e.target.innerText;                            // set new currentOperator.
         }
-        else if (mathValues.currentOperator !== '') { // operator been clicked after two operands and operator are already been clickd.
+        else if (mathValues.currentOperator !== ''){                                     // operator been clicked after two operands and operator are already been clickd.
             display.innerText = eval(display.innerText.replace(/[^-+/.*\d]/g, ''));
             mathValues.firstOperand = display.innerText;
             mathValues.secondOperand = '';
             display.innerText += e.target.id;
             mathValues.currentOperator = e.target.innerText;
         }
-    }
-    ;
-}
-;
+    };
+};
+
 let backBtn = byIdCalc('back');
 backBtn.addEventListener('click', () => {
     if (mathValues.scientific === false) {
@@ -84,23 +93,25 @@ backBtn.addEventListener('click', () => {
             mathValues.secondOperand = '';
             display.innerText = mathValues.firstOperand + mathValues.currentOperator;
         }
-    }
-    ;
+    };
 });
-let clearBtn = byIdCalc('C');
+
+let clearBtn = byIdCalc('C')
 clearBtn.addEventListener('click', () => {
     mathValues.firstOperand = '';
     mathValues.secondOperand = '';
     mathValues.currentOperator = '';
     display.innerText = '';
 });
-let equalBtn = byIdCalc('equal');
-equalBtn.addEventListener('click', () => {
+
+let equalBtn = byIdCalc('equal')
+equalBtn.addEventListener('click', () =>{
     display.innerText = eval(display.innerText.replace(/[^-+/.*\d]/g, ''));
     mathValues.valueCalculated = display.innerText;
 });
+
 let scientificMode = byIdCalc('scientific-mode');
-scientificMode.addEventListener('click', () => {
+scientificMode.addEventListener('click', () =>{
     display.innerText = '';
     mathValues = {
         scientific: true,
@@ -110,6 +121,7 @@ scientificMode.addEventListener('click', () => {
         valueCalculated: ''
     };
 });
+
 let basicMode = byIdCalc('basic-mode');
 basicMode.addEventListener('click', () => {
     display.innerText = '';
@@ -121,3 +133,4 @@ basicMode.addEventListener('click', () => {
         valueCalculated: ''
     };
 });
+
